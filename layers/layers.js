7dadc9454d1119a4502b1e7ade51a1a8d0496b51
dedev17 <devianasalsaaa@gmail.com>
2024-08-15 16:55:18 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,92 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_Waypoints_1 = new ol.format.GeoJSON();
+var features_Waypoints_1 = format_Waypoints_1.readFeatures(json_Waypoints_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Waypoints_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Waypoints_1.addFeatures(features_Waypoints_1);
+var lyr_Waypoints_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Waypoints_1, 
+                style: style_Waypoints_1,
+                popuplayertitle: "Waypoints",
+                interactive: false,
+                title: '<img src="styles/legend/Waypoints_1.png" /> Waypoints'
+            });
+var format_CILULUK_2 = new ol.format.GeoJSON();
+var features_CILULUK_2 = format_CILULUK_2.readFeatures(json_CILULUK_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_CILULUK_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_CILULUK_2.addFeatures(features_CILULUK_2);
+var lyr_CILULUK_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_CILULUK_2, 
+                style: style_CILULUK_2,
+                popuplayertitle: "CILULUK",
+                interactive: false,
+                title: '<img src="styles/legend/CILULUK_2.png" /> CILULUK'
+            });
+var format_Buffered_3 = new ol.format.GeoJSON();
+var features_Buffered_3 = format_Buffered_3.readFeatures(json_Buffered_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Buffered_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Buffered_3.addFeatures(features_Buffered_3);
+var lyr_Buffered_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Buffered_3, 
+                style: style_Buffered_3,
+                popuplayertitle: "Buffered",
+                interactive: false,
+                title: '<img src="styles/legend/Buffered_3.png" /> Buffered'
+            });
+var format_UTM_4 = new ol.format.GeoJSON();
+var features_UTM_4 = format_UTM_4.readFeatures(json_UTM_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_UTM_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_UTM_4.addFeatures(features_UTM_4);
+var lyr_UTM_4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_UTM_4, 
+                style: style_UTM_4,
+                popuplayertitle: "UTM",
+                interactive: false,
+                title: '<img src="styles/legend/UTM_4.png" /> UTM'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_Waypoints_1.setVisible(true);lyr_CILULUK_2.setVisible(true);lyr_Buffered_3.setVisible(true);lyr_UTM_4.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_Waypoints_1,lyr_CILULUK_2,lyr_Buffered_3,lyr_UTM_4];
+lyr_Waypoints_1.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_CILULUK_2.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_Buffered_3.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_UTM_4.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Waypoints_1.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr_CILULUK_2.set('fieldImages', {'KDPPUM': 'TextEdit', 'NAMOBJ': 'TextEdit', 'REMARK': 'TextEdit', 'KDPBPS': 'TextEdit', 'FCODE': 'TextEdit', 'LUASWH': 'TextEdit', 'UUPP': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDBPUM': 'TextEdit', 'WADMKD': 'TextEdit', 'WIADKD': 'TextEdit', 'WADMKC': 'TextEdit', 'WIADKC': 'TextEdit', 'WADMKK': 'TextEdit', 'WIADKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADPR': 'TextEdit', 'TIPADM': 'Range', 'SHAPE_Leng': 'TextEdit', 'SHAPE_Area': 'TextEdit', });
+lyr_Buffered_3.set('fieldImages', {'Name': 'TextEdit', 'descriptio': 'TextEdit', 'timestamp': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'altitudeMo': 'TextEdit', 'tessellate': 'TextEdit', 'extrude': 'TextEdit', 'visibility': 'TextEdit', 'drawOrder': 'TextEdit', 'icon': 'TextEdit', });
+lyr_UTM_4.set('fieldImages', {'Name': '', 'descriptio': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMo': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_Waypoints_1.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_CILULUK_2.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_Buffered_3.set('fieldLabels', {'Name': 'no label', 'descriptio': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_UTM_4.set('fieldLabels', {'Name': 'no label', 'descriptio': 'header label - always visible', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_UTM_4.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
